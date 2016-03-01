9cb5212dedb69d052e5618f5bceff0ef8ae2583a
mikey <mikeyjhavoc@gmail.com>
2016-03-01 06:04:46 -0500

    comments add, todos add for construction

---
+++ listFiles.py
@@ -0,0 +1,19 @@
+# listFiles
+# author: Michael Williams
+# date: 2/29/16
+# description: looks through a folder and lists all files contained in said
+#              folder and creates and lists them in a text document.
+# updated:
+# version: 0.5
+
+#TODO folder os walk folder, subdirectory, filenames
+
+#TODO create text file
+
+#TODO send filenames to text file.
+
+#TODO give file path for folder to os.walk (version 2)
+
+#TODO add yyyy-mm-dd-files.txt (version 3)
+
+#TODO be able to alter filename - yyyy-mm-dd-filename.txt (version 4)
