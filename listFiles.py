0461368dcb158954436a94f8b80c582c8cdd1533
mikey <mikeyjhavoc@gmail.com>
2016-03-02 08:11:14 -0500

    folder, subdirectory,filenames os.walk folder. counter descript output_files seperately. file_name for filenames search_folder takes fullpath

--- listFiles.py
+++ listFiles.py
@@ -10,11 +10,24 @@ import os,sys
 #Folder os walk folder, subdirectory, filenames
 def search_folder(folder):
     '''walks folder path from top node(folder) too nodes(files)'''
-    for folder, subdirectory, files in os.walk(folder):
+    for folder, subdirectory, filenames in os.walk(folder):
+        counter = 1 # increase output_file by 1
 
         folder = os.path.abspath(folder) # make sure it is the absolute path.
 
-        list_receiver = open('output_file','w')  # text file to write output list too.
+        list_receiver = open('output_file' + str(counter) + '.txt','w')  # text file to write output list too.  - w - creates text file if none exists.
+
+        #base_name = os.path.getcwd()
+
+
+        file_name = filenames
+
+        for file_name in filenames:
+            #if subdirectory.startswith('.git'):
+            #    continue
+            print(file_name)
+
+search_folder('C:\\Users\\mjwil\\documents\\github\\portfolio\\python\\listfiles')
 
 
 
