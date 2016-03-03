c56fe542decc3d07293aa62ece0cf02c96288c46
mikey <mikeyjhavoc@gmail.com>
2016-03-03 09:04:39 -0500

    update path file

--- listFiles.py
+++ listFiles.py
@@ -4,21 +4,23 @@
 # description: looks through a folder and lists all files contained in said
 #              folder and creates and lists them in a text document.
 # updated:
-# version: 0.5
+# version: 1.0
 import os,sys
 
 #Folder os walk folder, subdirectory, filenames
 def search_folder(folder):
     '''list directory of incoming path'''
     for filenames in os.listdir(folder):
+
      print(filenames)
-
-search_folder('C:\\Users\\mjwil\\documents\\github\\portfolio\\python\\listfiles')
-
 
 #TODO send filenames to text file.
 #list_receiver = open('output_file' + str(counter) + '.txt','w')  # text file to write output list too.  - w - creates text file if none exists.
 
+
+
+path = os.getcwd()
+search_folder(path)
 #TODO give file path for folder to os.walk (version 2)
 
 #TODO add yyyy-mm-dd-files.txt (version 3)
